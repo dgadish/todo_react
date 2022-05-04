fc0ae1a57fcb9ac76e4fb4135378bd17c4ae50b6
Daniel Gadish <dgadish99@gmail.com>
2022-05-04 10:55:18 +0100

    Fix for error with task filter function - changed default filter state from 'All' to 'ALL'

--- src/App.js
+++ src/App.js
@@ -18,40 +18,7 @@ const FILTER_NAMES = Object.keys(FILTER_MAP);
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks); //preserves initial state of props.tasks from index.js
-  const [filter, setFilter] = useState('All'); // set default state for tasks filter
-  
-  // create list of tasks based on <Todo /> component and the tasks rendered in index.js
-  const taskList = tasks
-  .filter(FILTER_MAP[filter])
-  .map(task => (
-    <Todo 
-      id={task.id} 
-      name={task.name} 
-      completed={task.completed}
-      key={task.id} 
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  ));
-
-  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'; // if taskList.length is 1, use 'task', otherwise use 'tasks'
-  const headingText = `${taskList.length} ${tasksNoun} remaining`; // variable for dynamic list length count
-
-  const filterList = FILTER_NAMES.map(name => (
-    <FilterButton 
-      key={name} 
-      name={name}
-      isPressed={name === filter}
-      setFilter={setFilter}
-    />
-  ));
-
-  // add task to the task list
-  function addTask(name) {
-    const newTask = { id: "todo-" + nanoid(), name: name, completed: false }; // creates the new task
-    setTasks([...tasks, newTask]); // adds new task to tasks array ('...tasks' copies existing array 'tasks')
-  }
+  const [filter, setFilter] = useState('ALL'); // set default state for tasks filter
   
   // toggle completed state
   function toggleTaskCompleted(id) {
@@ -84,9 +51,41 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
+  // create list of tasks based on <Todo /> component and the tasks rendered in index.js
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
+    <Todo 
+      id={task.id} 
+      name={task.name} 
+      completed={task.completed}
+      key={task.id} 
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  // add task to the task list
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false }; // creates the new task
+    setTasks([...tasks, newTask]); // adds new task to tasks array ('...tasks' copies existing array 'tasks')
+  }
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'; // if taskList.length is 1, use 'task', otherwise use 'tasks'
+  const headingText = `${taskList.length} ${tasksNoun} remaining`; // variable for dynamic list length count
+
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
       <Form addTask={addTask} /> {/* Create input form, utilising the 'addTask' function to allow the user input to be passed as a prop to <Form /> */}
       <div className="filters btn-group stack-exception">
         {filterList}
--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ const DATA = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App subject="Rachel" tasks={DATA} />
+    <App tasks={DATA} />
   </React.StrictMode>
 );

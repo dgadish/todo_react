ca3abb44e09e39c5ee93403032f749717ba08f26
Daniel Gadish <dgadish99@gmail.com>
2022-05-04 09:52:04 +0100

    Filter functionality added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,48 @@ import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import { nanoid } from 'nanoid'; 
 
+// filter functions defined outside of app as these will not change, don't want to recalculate every time the app loads
+// create function mapping for the filters
+const FILTER_MAP = {
+  ALL: () => true,
+  ACTIVE: task => !task.completed,
+  COMPLETED: task => task.completed
+};
+
+// collect an array of filter names
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks); //preserves initial state of props.tasks from index.js
+  const [filter, setFilter] = useState('All'); // set default state for tasks filter
+  
+  // create list of tasks based on <Todo /> component and the tasks rendered in index.js
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
+    <Todo 
+      id={task.id} 
+      name={task.name} 
+      completed={task.completed}
+      key={task.id} 
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'; // if taskList.length is 1, use 'task', otherwise use 'tasks'
+  const headingText = `${taskList.length} ${tasksNoun} remaining`; // variable for dynamic list length count
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
 
   // add task to the task list
   function addTask(name) {
@@ -46,30 +84,12 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
-  // create list of tasks based on <Todo /> component and the tasks rendered in index.js
-  const taskList = tasks.map(task => (
-    <Todo 
-      id={task.id} 
-      name={task.name} 
-      completed={task.completed}
-      key={task.id} 
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  ));
-
-  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'; // if taskList.length is 1, use 'task', otherwise use 'tasks'
-  const headingText = `${taskList.length} ${tasksNoun} remaining`; // variable for dynamic list length count
-
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} /> {/* Create input form, utilising the 'addTask' function to allow the user input to be passed as a prop to <Form /> */}
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
